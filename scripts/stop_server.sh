7ebc4517537160bd932f837d5e5f4cafb1401d35
rahul-yadav-hub <rahuly850@gmail.com>
2022-03-25 14:55:52 +0530

    Updated

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo "stop"
+
+pid=$(sudo pm2 pid conduit)
+if [ $pid == 0 ]; then
+   echo "Application is stopped"
+else
+   echo "Application is running"
+   sudo pm2 stop conduit/ecosystem.config.js
+
