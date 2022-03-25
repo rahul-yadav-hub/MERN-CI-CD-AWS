19cdb5a0792ec790e460fbaef8033ba4b566fb8f
rahul-yadav-hub <rahuly850@gmail.com>
2022-03-25 12:52:02 +0530

    Updated ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,8 +5,8 @@ module.exports = {
               script: "./app.js",
               watch: true,
               env: {
-                  "MONGODB_DEV_URI": "your_mongodb_uri_here",
-                  "PORT": your_app_port_here
+                  "PORT": your_app_port_here,
+                  "MONGODB_DEV_URI": "your_mongodb_uri_here"
               }
             }
        ]
